c0507988b087993aea0c93b434ecf30f8da622bf
Eloisa da Silva <edasilva@ingsw.com>
2017-11-23 16:35:33 -0300

    ember-cli-board-calendar: wip

--- addon/components/ebc-header.js
+++ addon/components/ebc-header.js
@@ -4,5 +4,10 @@ import layout from '../templates/components/ebc-header';
 export default Ember.Component.extend({
   layout,
   classNames: ['ebc-header'],
-  columns: null
+  columns: null,
+
+  init() {
+    this._super(...arguments);
+    console.log('COLUMNS COUNT', this.get('columns.firstObject'));
+  }
 });
