d95186fe0ce4962a845489d1ead4fc69c329eef5
Eloisa da Silva <edasilva@ingsw.com>
2017-11-27 18:14:36 -0300

    ecb-calendar: styles and custom properties

--- addon/components/ebc-column.js
+++ addon/components/ebc-column.js
@@ -5,5 +5,11 @@ import layout from '../templates/components/ebc-column';
 export default Ember.Component.extend({
   classNames: ['ebc-column'],
   layout,
-  column: null
+  column: null,
+  attributeBindings: ['customStyle:style'],
+  customStyle: computed('column.width', {
+    get() {
+      return `min-width:${this.get('column.width')};`
+    }
+  })
 });
--- addon/components/ebc-header-cell.js
+++ addon/components/ebc-header-cell.js
@@ -1,7 +1,14 @@
 import Component from '@ember/component';
 import layout from '../templates/components/ebc-header-cell';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
-  classNames: ['ebc-header-cell']
+  classNames: ['ebc-header-cell'],
+  attributeBindings: ['customStyle:style'],
+  customStyle: computed('columnWidth', {
+    get() {
+      return `min-width:${this.get('columnWidth')};`
+    }
+  })
 });
