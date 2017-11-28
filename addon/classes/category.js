7dd093f886382816b4721527e0912cf90544d9e1
Nicolas Torres <ntorres@ingsw.com>
2017-11-28 16:35:44 -0300

    Removes log

--- addon/classes/category.js
+++ addon/classes/category.js
@@ -1,8 +1,6 @@
 import EmberObject, { computed } from '@ember/object';
 import { A } from '@ember/array';
 
-
-
 export default class Category extends EmberObject.extend({
   classNames: [],
   categoryId: null,
@@ -16,7 +14,5 @@ export default class Category extends EmberObject.extend({
     super();
     this.setProperties(options);
     this.set('divisions', categoryDivisions);
-
-    console.log('CATEGORY DETECTED', this.get('label'));
   }
 }
